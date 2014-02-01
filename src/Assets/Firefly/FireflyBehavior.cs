26590a05a6339e4ac62be01bd5442a59a7d322b2
Robert Simmons <robert.simmons.tn@gmail.com>
2014-02-01 16:52:31 -0600

    Clicking now KILLS 'EM DEAD!!!

---
+++ src/Assets/Firefly/FireflyBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireflyBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown(){
+		Destroy (transform.gameObject);
+	}
+}
