dca7bc6829c02cec2ddbff86b71a0b67103bc5d6
Robert Simmons <robert.simmons.tn@gmail.com>
2014-02-01 15:37:18 -0600

    Fireflies spawn now

---
+++ src/Assets/FireflySpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireflySpawner : MonoBehaviour {
+	
+	public float spawnTime = 0.03f;		// The amount of time between each spawn.
+	public float spawnDelay = 0f;		// The amount of time before spawning starts.
+
+	public GameObject firefly;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+	
+	void Spawn ()
+	{
+		int shouldSpawn = Random.Range(0, 10);
+
+		if (shouldSpawn > 2) {
+			Vector3 where = new Vector3(Random.Range(-6f, 6f), Random.Range(-5f, 6f), 0);
+
+			Instantiate(firefly, where, transform.rotation);
+		}
+	}
+}
